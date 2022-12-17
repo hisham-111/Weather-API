617070f135972a3d227032b09c4d3929e6d8f667
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-17 16:56:43 +0200

    Added my name to the app!

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name: "Karim"
+      name: "Hisham"
     };
   }
 
@@ -22,7 +22,7 @@ class App extends Component {
     return (
       <div className="app">
         <SayHi />
-        <SayHello color="black" name={this.state.name} />
+        <SayHello color="blue" name={this.state.name} />
         <Search handleInput={this.handleInputChange} />
       </div>
     );
