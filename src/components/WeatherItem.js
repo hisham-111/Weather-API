c9252aacaba0e85db479ab5154f2bd1be3d4c389
Hisham El Ahdab <hisham7@Hisham.HishamElahdab>
2022-12-19 16:26:15 +0200

    Weather API: HTML/CSS

--- src/components/WeatherItem.js
+++ src/components/WeatherItem.js
@@ -3,14 +3,24 @@ import { render } from "react-dom";
 
 export default class WeatherItem extends Component {
   render() {
-    return <div>Heello</div>;
+    return <div></div>;
   }
 }
+// export class SayHello extends Component {
+//   render() {
+//     return (
+//       <div style={{ color: this.props.color, backgroundColor: "blue" }}>
+//         Hello {this.props.name}
+//       </div>
+//     );
+//   }
+// }
+
 export class SayHello extends Component {
   render() {
     return (
-      <div style={{ color: this.props.color, backgroundColor: "yellow" }}>
-        Hello {this.props.name}
+      <div className="App__search">
+       
       </div>
     );
   }
